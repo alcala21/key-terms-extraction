8495b666e07ffc94b562be7262c4ed073dd41358
Carlos Alcala <alcala21@gmail.com>
2021-11-17 16:50:21 -0600

    Improved Stage 1/4. Key Terms Extraction.

--- key_terms.py
+++ key_terms.py
@@ -8,8 +8,7 @@ nd = dict()
 
 for news in root[0]:
     head = news[0].text
-    tokens = nltk.word_tokenize(news[1].text.lower())
-    cd = sorted(Counter(tokens).items(), reverse=True, key=lambda x: (x[1], x[0]))
-    top_five = " ".join([x[0] for x in cd[:5]])
+    tokens = sorted(nltk.word_tokenize(news[1].text.lower()), reverse=True)
+    cd = Counter(tokens).most_common(5)
+    top_five = " ".join(x[0] for x in cd)
     print(head + ":", top_five, sep="\n", end='\n\n')
-
