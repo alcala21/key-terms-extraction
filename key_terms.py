abf95c0035f47fb63cd7fca0ccac78b20e978bb8
Carlos Alcala <alcala21@gmail.com>
2021-11-17 19:54:42 -0600

    Stage 2/4. Key Terms Extraction.

--- key_terms.py
+++ key_terms.py
@@ -2,13 +2,21 @@
 from lxml import etree
 import nltk
 from collections import Counter
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
+import re
 
 root = etree.parse("news.xml").getroot()
 nd = dict()
+lemmatize = WordNetLemmatizer().lemmatize
+not_cool = stopwords.words("English") + list(string.punctuation)
 
 for news in root[0]:
     head = news[0].text
     tokens = sorted(nltk.word_tokenize(news[1].text.lower()), reverse=True)
+    tokens = [lemmatize(word) for word in tokens]
+    tokens = [word for word in tokens if word not in not_cool]
     cd = Counter(tokens).most_common(5)
     top_five = " ".join(x[0] for x in cd)
     print(head + ":", top_five, sep="\n", end='\n\n')
